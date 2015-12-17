75c6f05c53fad80ba8296c2506292015321a90d8
akzReekoh <achilles.dinglasan@p3ople4u.com>
2015-12-17 17:16:14 +0800

    REEKOH-506
    -Finished sending of data to Apache ActiveMQ
    -Finished documentation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# activemq-connector
-Integrates a Reekoh instance to Apache ActiveMQ messaging service
+# Apache ActiveMQ Connector
+[![Build Status](https://travis-ci.org/Reekoh/activemq-connector.svg)](https://travis-ci.org/Reekoh/activemq-connector)
+![Dependencies](https://img.shields.io/david/Reekoh/activemq-connector.svg)
+![Dependencies](https://img.shields.io/david/dev/Reekoh/activemq-connector.svg)
+![Built With](https://img.shields.io/badge/built%20with-gulp-red.svg)
+
+Integrates a Reekoh instance to Apache ActiveMQ messaging service.
+
+## Description
+This plugin sends messages from devices connected to Reekoh Instance to Apache ActiveMQ.
+
+## Configuration
+To configure this plugin, a running Apache ActiveMQ service is needed to provide the following:
+
+1. Host - The IP/Domain in which the Apache ActiveMQ service is running.
+2. Username - The Apache ActiveMQ account username to use.
+3. Password -  The Apache ActiveMQ account password to use.
+4. Port - The Port where Apache ActiveMQ is running.
+
+Other Parameters:
+
+1. Default Message - The default message to be sent.
+2. Default Message Type - The default type of the message to be sent.
+3. Default Topic/Queue Name - The default topic or queue where the message will be sent.
+
+These parameters are then injected to the plugin from the platform.
+
+## Sample input data
+```
+{
+    message_type: 'queue', //message type 
+    message: 'This is a test message from Apache ActiveMQ Plugin.',
+    topic_queue_name: 'test' //topic or queue name
+}
+```
---
+++ app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var platform = require('./platform'),
+    request = require('request'),
+    isEmpty = require('lodash.isempty'),
+    isPlainObject = require('lodash.isplainobject'),
+	config;
+
+platform.on('data', function (data) {
+    if(isPlainObject(data)){
+        if(isEmpty(data.message_type))
+            data.message_type = config.default_message_type;
+
+        if(data.message_type.toLowerCase() !== 'queue' && data.message_type.toLowerCase() !== 'topic')
+            data.message_type = config.default_message_type;
+
+        if(isEmpty(data.message))
+            data.message = config.default_message;
+
+        if(isEmpty(data.topic_queue_name))
+            data.topic_queue_name = config.topic_queue_name;
+
+        //sanitize the host
+        var host;
+        if (config.host.indexOf('://') > -1) {
+            host = config.host.split('/')[2];
+        }
+        else {
+            host = config.host.split('/')[0];
+        }
+        host = host.split(':')[0];
+
+        var url = 'http://'+ config.username +':'+ config.password +'@'+ host +':'+ config.port +'/api/message/'+ data.topic_queue_name +'?type='+ data.message_type.toLowerCase();
+
+        request.post({
+            url: url,
+            body: JSON.stringify(data.message)
+        }, function (error) {
+            if (error)
+                platform.handleException(error);
+            else {
+                platform.log(JSON.stringify({
+                    title: 'Data sent to Apache ActiveMQ.',
+                    data: data
+                }));
+            }
+        });
+    }
+    else
+        platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data: ' + data));
+
+
+});
+
+platform.once('close', function () {
+    platform.notifyClose();
+});
+
+platform.once('ready', function (options) {
+    config = options;
+
+	platform.log('Apache ActiveMQ Connector has been initialized.');
+    platform.notifyReady();
+});
