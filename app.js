2065643073fae18f2e9a8159bae7f9012660f314
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-03-08 19:10:15 +0800

    REEKOH-506
    -Updated travis.yml
    -Updated app.js to accept array input
    -Bumped version
    -Updated test suite to test array input

--- app.js
+++ app.js
@@ -3,51 +3,62 @@
 var platform = require('./platform'),
     request = require('request'),
     isEmpty = require('lodash.isempty'),
+    isArray = require('lodash.isarray'),
+    async = require('async'),
     isPlainObject = require('lodash.isplainobject'),
 	config;
 
+let sendData = (data) => {
+    if(isEmpty(data.message_type))
+        data.message_type = config.default_message_type;
+
+    if(data.message_type.toLowerCase() !== 'queue' && data.message_type.toLowerCase() !== 'topic')
+        data.message_type = config.default_message_type;
+
+    if(isEmpty(data.message))
+        data.message = config.default_message;
+
+    if(isEmpty(data.topic_queue_name))
+        data.topic_queue_name = config.topic_queue_name;
+
+    //sanitize the host
+    var host;
+    if (config.host.indexOf('://') > -1) {
+        host = config.host.split('/')[2];
+    }
+    else {
+        host = config.host.split('/')[0];
+    }
+    host = host.split(':')[0];
+
+    var url = 'http://'+ config.username +':'+ config.password +'@'+ host +':'+ config.port +'/api/message/'+ data.topic_queue_name +'?type='+ data.message_type.toLowerCase();
+
+    request.post({
+        url: url,
+        body: JSON.stringify(data.message)
+    }, function (error) {
+        if (error)
+            platform.handleException(error);
+        else {
+            platform.log(JSON.stringify({
+                title: 'Data sent to Apache ActiveMQ.',
+                data: data
+            }));
+        }
+    });
+};
+
 platform.on('data', function (data) {
     if(isPlainObject(data)){
-        if(isEmpty(data.message_type))
-            data.message_type = config.default_message_type;
-
-        if(data.message_type.toLowerCase() !== 'queue' && data.message_type.toLowerCase() !== 'topic')
-            data.message_type = config.default_message_type;
-
-        if(isEmpty(data.message))
-            data.message = config.default_message;
-
-        if(isEmpty(data.topic_queue_name))
-            data.topic_queue_name = config.topic_queue_name;
-
-        //sanitize the host
-        var host;
-        if (config.host.indexOf('://') > -1) {
-            host = config.host.split('/')[2];
-        }
-        else {
-            host = config.host.split('/')[0];
-        }
-        host = host.split(':')[0];
-
-        var url = 'http://'+ config.username +':'+ config.password +'@'+ host +':'+ config.port +'/api/message/'+ data.topic_queue_name +'?type='+ data.message_type.toLowerCase();
-
-        request.post({
-            url: url,
-            body: JSON.stringify(data.message)
-        }, function (error) {
-            if (error)
-                platform.handleException(error);
-            else {
-                platform.log(JSON.stringify({
-                    title: 'Data sent to Apache ActiveMQ.',
-                    data: data
-                }));
-            }
+        sendData(data);
+    }
+    else if(isArray(data)){
+        async.each(data, (datum) => {
+            sendData(datum);
         });
     }
     else
-        platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data: ' + data));
+        platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data: ' + data));
 
 
 });
